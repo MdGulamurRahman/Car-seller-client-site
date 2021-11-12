8c707f8e1b427df621d27b72575e9580a6fd5bfd
Abrarhusamfahim <fahimjahin321@gmail.com>
2021-11-12 17:32:17 +0600

    added new code

--- src/Pages/Home/Ratings/Ratings.js
+++ src/Pages/Home/Ratings/Ratings.js
@@ -1,7 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Card, CardGroup, Row } from 'react-bootstrap';
-import Rating from 'react-rating';
-import Zoom from 'react-reveal/Zoom';
 const Ratings = () => {
     const [reviews, setReviews] = useState([]);
     useEffect(()=>{
@@ -10,39 +7,70 @@ const Ratings = () => {
         .then(data => setReviews(data))
     },[])
     return (
-        <div className="pb-5 container-fluid">
-            <h1 className="pt-5 fw-bold text-dark"><span>MANAGE YOUR PRODUCTS</span></h1>
-            <h6 className="pt-2 pb-4 all-clr"><small>Find and Compare Great Car Deals!</small></h6>
-            <div className="container">
-            <Row xs={1} md={4} className="g-2">
-            {
-               reviews.map(pd => <CardGroup>
-                    <Zoom>
-                 <Card className="border-0">
-                    <div className="bg-image hover-zoom">
-                    <Card.Img style={{ borderRadius: "50%"}} variant="top" src={pd.img} />
-                    </div>
-                    <Card.Body>
-                    <Card.Title className="fw-bold text-success">{pd.name}</Card.Title>
-                    
-                    <Card.Text className="text-secondary">
-                        {pd.reviewDetails}
-                    </Card.Text>
-                    </Card.Body>
-                    <Card.Footer className="p-3 border-0 d-flex align-items-center justify-content-between">
-                        Rating:
-                    <Rating
-                    initialRating={pd.rating}
-                    readonly
-                    emptySymbol="far fa-star-o fa-2x"
-                    fullSymbol="fas fa-star fa-2x"
-                    />
-                    </Card.Footer>
-                </Card>
-                </Zoom>
-                </CardGroup>)
-            }
-            </Row>
+        <div>
+            <h2>Our Clients Reviews</h2>
+            <div className="container ratings">
+                <div className="row">
+                    {
+                        reviews.map(review => (
+                            <>
+                                <div className="mb-3 col-12 col-md-3">
+                                    <img src={review?.img}
+                                        width="100"
+                                        height="100"
+                                        className="rounded-circle" alt="" />
+                                    <h4>{review?.name}</h4>
+                                    <p style={{ fontWeight: 600 }}>{review?.reviewDetails}</p>
+                                    {   
+                                        review?.ratings==="5"?
+                                        <h6>Ratings: <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        </h6>: ""
+                                        
+                                    }
+                                    {   
+                                        review?.ratings==="4"?
+                                        <h6>Ratings: <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        
+                                        </h6>: ""
+                                        
+                                    }
+                                    {   
+                                        review?.ratings==="3"?
+                                        <h6>Ratings: <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        
+                                        </h6>: ""
+                                        
+                                    }
+                                    {   
+                                        review?.ratings==="2"?
+                                        <h6>Ratings: <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        </h6>: ""
+                                        
+                                    }
+                                    {   
+                                        review?.ratings==="1"?
+                                        <h6>Ratings: <i class="fas fa-star"></i>
+                                        </h6>: ""
+                                        
+                                    }
+                                </div>
+
+                            </>
+                        ))
+                    }
+
+
+                </div>
             </div>
         </div>
     );
